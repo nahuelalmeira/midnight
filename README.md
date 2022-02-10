aab8e7eb82a9146da2ecab420274264f236d1669
nahuelalmeira <nahu1990@gmail.com>
2022-02-10 05:24:20 -0300

    Add game tests

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # midnight
+
+## Simulator of the [Midnight](https://en.wikipedia.org/wiki/Midnight_(game)) game.
+
+Create players, defining their strategy
+
+```
+player1 = Player(strategy=AlwaysConservativeStrategy())
+player2 = Player(strategy=AlwaysConservativeStrategy())
+```
+
+Create a game, defining the number of rounds that will be played.
+
+```
+game = Game(n_rounds=10)
+```
+
+Add players
+
+```
+game.add_player(player1)
+game.add_player(player2)
+```
+
+Run simulation
+
+```
+game.play()
+```
+
+Get game satistics as a `pandas.DataFrame`
+
+```
+game.get_game_stats()
+```
+
+Get players' scores as a `pandas.DataFrame`
+
+```
+game.get_all_scores()
+```
---
+++ tests/test_game.py
@@ -0,0 +1,27 @@
+from unittest.mock import Mock
+import pytest
+from midnight.main import Game, NoPlayerException
+
+
+class TestGame:
+    def test_no_players_raises_exception(self):
+        game = Game()
+        with pytest.raises(NoPlayerException):
+            game.play()
+
+    def test_sort_players(self):
+        game = Game()
+        game.players = ["Player1", "Player2", "Player3"]
+        game.round_first_player = 1
+        assert game.sort_players() == ["Player2", "Player3", "Player1"]
+
+    def test_top_score(self):
+        game = Game()
+        player1 = Mock()
+        player1.score = 5
+        player2 = Mock()
+        player2.score = 2
+        player3 = Mock()
+        player3.score = 7
+        game.players = [player1, player2, player3]
+        assert game.top_score == 7
